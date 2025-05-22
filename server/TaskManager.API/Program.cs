42f3b68fc31e99eddc3ef38c144422147dda119d
Rehan Shami <rehanshami94@gmail.com>
2025-05-23 01:42:01 +1200

    feat: setup EF Core with SQL Server and Docker

--- server/TaskManager.API/Program.cs
+++ server/TaskManager.API/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using TaskManager.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
