c86f6f659d27925d13a2584c5a8eae8d274d8c4a
Rehan Shami <rehanshami94@gmail.com>
2025-05-23 13:22:30 +1200

    feat: Build simple GET task route to test DB connection

---
+++ server/TaskManager.API/Controllers/TasksController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.API.Data;
+using TaskManager.API.Models;
+
+namespace TaskManager.API.AddControllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public TasksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetTasks()
+        {
+            var tasks = await _context.Tasks.ToListAsync();
+            return Ok(tasks);
+        }
+    }
+}
